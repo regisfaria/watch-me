08c040390a4dd390b808f5694e0006c8735febd3
regisfaria <rgisfaria@yahoo.com.br>
2021-06-08 18:03:09 -0300

    refactored from app into a new component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,27 @@
+import { Button } from '../components/Button';
+
+import { useGenre } from '../hooks/genreContext';
+
+import '../styles/sidebar.scss';
+
 export function SideBar() {
-  // Complete aqui
+  const { genres, handleSelectedGenreIdChange, selectedGenreId } = useGenre();
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleSelectedGenreIdChange(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
